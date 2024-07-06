ac9406116621914463386a9e0396cafb40247f07
Maksym Nov <novodmaxim@gmail.com>
2024-07-06 12:08:19 +0200

    added models: Task and Comment, done migrations

---
+++ task_manager/tasks/models.py
@@ -0,0 +1,38 @@
+# tasks/models.py
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('P', 'Pending'),
+        ('C', 'Completed'),
+        ('F', 'Failed'),
+    ]
+
+    PRIORITY_CHOICES = [
+        ('L', 'Low'),
+        ('M', 'Medium'),
+        ('H', 'High'),
+    ]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES, default='low')
+    deadline = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    task = models.ForeignKey(Task, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.task.title}'
+
